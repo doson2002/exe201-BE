d087bc590ecd86d1548ec4853bc6515ad09e4228
doson200220 <153179873+doson200220@users.noreply.github.com>
2024-09-08 09:49:50 +0700

    Test commit

---
+++ src/main/java/com/exe201/exe201be/Exe201beApplication.java
@@ -0,0 +1,13 @@
+package com.exe201.exe201be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exe201beApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exe201beApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exe201/exe201be/Exe201beApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe201.exe201be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exe201beApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
